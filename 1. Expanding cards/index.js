4335a5315f366b8f2ad90967160fad776a1e46a4
Jelena Milanovic <jelena.m@eyesee-research.com>
2020-12-11 19:36:26 +0100

    Day 1: Expanding cards

---
+++ 1. Expanding cards/index.js
@@ -0,0 +1,18 @@
+let cards = null;
+let currentActiveIndex = 0;
+
+window.onload = () => initCards();
+
+function initCards() {
+  cards = document.getElementsByClassName('card');
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', () => setActiveCard(i))
+  }
+}
+
+function setActiveCard(i) {
+  console.log('card is active', i)
+  cards[currentActiveIndex].classList.remove('active');
+  cards[i].classList.add('active');
+  currentActiveIndex = i;
+}
