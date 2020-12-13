5c5e2cf306a4b24400f2df38d8fbb3f730c0fcdd
Jelena Milanovic <jmilanovic114@gmail.com>
2020-12-13 13:11:58 +0100

    5. Blurry Loading

---
+++ 5. Blurry Loading/index.js
@@ -0,0 +1,21 @@
+let wrapperEl, statusEl;
+
+window.onload = () => {
+  statusEl = document.querySelector(".loading-status");
+  let progress = 1;
+  let progressOpacity = 1;
+  let filterAmount = 100;
+
+
+  let progressInterval = setInterval(() => {
+    progress++;
+    progressOpacity -= 0.01;
+    filterAmount -= 1;
+    if (progress == 100) {
+      window.clearInterval(progressInterval);
+    }
+    statusEl.style.opacity = `${progressOpacity}`;
+    statusEl.innerText = `${progress}%`;
+    document.body.style.backdropFilter = `blur(${filterAmount}px)`;
+  }, 25);
+}
